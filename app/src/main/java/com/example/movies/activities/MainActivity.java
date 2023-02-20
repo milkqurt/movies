61cdee58f09f1ddd563e30f04dc8a7c88152f757
milkqurt <diasermekbaev342@gmail.com>
2023-02-20 16:22:31 +0600

    movies

---
+++ app/src/main/java/com/example/movies/activities/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.movies.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.movies.R;
+import com.example.movies.data.MovieAdapter;
+import com.example.movies.model.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private MovieAdapter movieAdapter;
+    private ArrayList<Movie> movies;
+    private RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recycclerView);
+        recyclerView.hasFixedSize();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        movies = new ArrayList<>();
+        requestQueue = Volley.newRequestQueue(this);
+        getMovies();
+    }
+
+    private void getMovies() {
+        String url = "https://www.omdbapi.com/?apikey=61fee442&s=horror";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET,
+                url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("Search");
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        String title = jsonObject.getString("Title");
+                        String year = jsonObject.getString("Year");
+                        String posterUrl = jsonObject.getString("Poster");
+                        Movie movie = new Movie();
+                        movie.setTitle(title);
+                        movie.setYear(year);
+                        movie.setPosterUrl(posterUrl);
+                        movies.add(movie);
+                    }
+                    movieAdapter = new MovieAdapter(MainActivity.this, movies);
+                    recyclerView.setAdapter(movieAdapter);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        requestQueue.add(request);
+    }
+}
---
+++ app/src/main/java/com/example/movies/data/MovieAdapter.java
@@ -0,0 +1,65 @@
+package com.example.movies.data;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.movies.R;
+import com.example.movies.model.Movie;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+
+    private Context context;
+    private ArrayList<Movie> movies;
+
+    public MovieAdapter(Context context, ArrayList<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+    @NonNull
+    @androidx.annotation.NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull @androidx.annotation.NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.movie_item, viewGroup, false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @androidx.annotation.NonNull MovieViewHolder movieViewHolder, int i) {
+        Movie currentMovie = movies.get(i);
+        String title = currentMovie.getTitle();
+        String year = currentMovie.getYear();
+        String posterUrl = currentMovie.getPosterUrl();
+        movieViewHolder.titleTextView.setText(title);
+        movieViewHolder.yearTextView.setText(year);
+        Picasso.get().load(posterUrl).fit().centerInside().into(movieViewHolder.posterImageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView posterImageView;
+        TextView titleTextView;
+        TextView yearTextView;
+
+        public MovieViewHolder(@NonNull @androidx.annotation.NonNull View itemView) {
+            super(itemView);
+            posterImageView = itemView.findViewById(R.id.posterImageView);
+            titleTextView = itemView.findViewById(R.id.titleTextView);
+            yearTextView = itemView.findViewById(R.id.yearTextView);
+        }
+    }
+}
